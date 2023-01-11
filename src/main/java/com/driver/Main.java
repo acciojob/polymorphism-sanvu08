93bcc46d36283ecaaf310ddd993e4ece6ffbab27
Sanvu Sarvesh <96696724+sanvu08@users.noreply.github.com>
2023-01-11 17:24:32 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,29 @@
 package com.driver;
 
-public class Main {
-
+import java.util.*;
+public class Main{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = sc.nextInt();
+        Product p = new Product();
+        int ans_1 = p.product(x,y);
+        int ans_2 = p.product(x,y,z);
+        double ans_3 = p.product((double)x, (double)y);
+        System.out.println(ans_1);
+        System.out.println(ans_2);
+        System.out.println(ans_3);
+    }
+    static class Product{
+      public static int product  (int x, int y){
+          return x*y;
+      }
+      public static int product (int x, int y, int z){
+          return x*y*z;
+      }
+      public static double product (double x, double y){
+          return x*y;
+      }
+    }
 }
